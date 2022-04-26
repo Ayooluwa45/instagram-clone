142efa759e8f1ec4a61376d9c9ab300fe27f672e
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-04-26 09:31:11 +0200

    Changes made

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,22 +1,30 @@
-import React from "react";
-import './Post.css'
+import "./Post.css";
 import { Avatar } from "@mui/material";
 
-function Post() {
+function Post({ username, imageUrl, caption }) {
   return (
     <div className="post">
-<div className="postHeader">
-<Avatar className="postAvatar" alt='Sanni emmanuel' src='/static/images/avatar/1.jpg'/>
-      <h3>Username</h3>
-</div>
-        
+      <div className="postHeader">
+        <Avatar
+          className="postAvatar"
+          alt="Sanni emmanuel"
+          src="/static/images/avatar/1.jpg"
+        />
+        <h3>{username}</h3>
+      </div>
+
       {/*header -- avatar + username  */}
 
-      <img className="postImage" src="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png" alt="" />
+      <img
+        className="postImage"
+        src={imageUrl}
+        alt=""
+      />
       {/*image  */}
 
-      <h4 className="postText"><strong>sanniemmanuel:</strong> I love my mom</h4>
-      {/*username + caption  */}
+      <h4 className="postText">
+        <strong>{username}</strong> {caption}
+      </h4>
     </div>
   );
 }
